153ce7e3ddce43242ab72055a05a6ad598830ceb
DaeSeok_Song <zxdsaqwe1231@naver.com>
2021-07-09 00:04:55 +0900

    WriteDiary TextField(Title complete, content -ing)

--- lib/UserPage.dart
+++ lib/UserPage.dart
@@ -1,3 +1,4 @@
+import 'package:dreamiary/WriteDiary.dart';
 import 'package:flutter/material.dart';
 
 class DreamiaryHome extends StatelessWidget {
@@ -10,89 +11,103 @@ class DreamiaryHome extends StatelessWidget {
         appBar: AppBar(
           title: Text("꿈일기"),
         ),
-        body: Column(
-          children: [
-            Container(
-              height: 150,
-              color: Colors.amber,
+        body: Container(
+          child: Column(
+            children: [
+              Container(
+                height: 150,
+                color: Colors.amber,
 
-              child: ListView.builder(
-                scrollDirection: Axis.horizontal,
-                itemCount: test.length, // ONLY effected on itemBuilder
+                child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: test.length, // ONLY effected on itemBuilder
 
-                itemBuilder: (BuildContext context, int index) {
-                  return Container(
-                    margin: const EdgeInsets.all(8),
+                  itemBuilder: (BuildContext context, int index) {
+                    return Container(
+                      margin: const EdgeInsets.all(8),
 
-                    child: ButtonTheme(
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(8),
+                      child: ButtonTheme(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+
+                        child: FlatButton(
+                            onPressed: () {},
+                            color: Colors.amberAccent,
+                            child: Text("일기 ${index + 1}. ${test[index]}")),
                       ),
-
-                      child: RaisedButton(
-                          onPressed: () {},
-                          color: Colors.amberAccent,
-                          child: Text("일기 ${index + 1}. ${test[index]}")),
-                    ),
-                  );
-                },
+                    );
+                  },
+                ),
               ),
-            ),
 
-            Expanded(child: Container(
-              
-            )),
+              Expanded(child: Container(
 
-            Container(
-              height: 50,
-              width: MediaQuery.of(context).size.width,
-              color: Colors.amber,
+              )),
 
-              child: ListView.separated(
-                physics: const NeverScrollableScrollPhysics(),
-                scrollDirection: Axis.horizontal,
-                itemCount: menu.length,
-                shrinkWrap: true,
+              Container(
+                height: 50,
+                width: MediaQuery.of(context).size.width,
+                color: Colors.amber,
 
-                itemBuilder: (BuildContext context, int index) {
-                  return Container(
-                    width: MediaQuery.of(context).size.width / menu.length,
+                child: ListView.separated(
+                  physics: const NeverScrollableScrollPhysics(),
+                  scrollDirection: Axis.horizontal,
+                  itemCount: menu.length,
+                  shrinkWrap: true,
 
-                    child: ButtonTheme(
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(0),
-                      ),
-
-                      child: RaisedButton(
-                        onPressed: () {},
-                        color: Colors.amber,
-                        child: Text(
-                          "${menu[index]}",
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 11,
+                  itemBuilder: (BuildContext context, int index) {
+                    return Container(
+                      width: MediaQuery.of(context).size.width / menu.length,
+                      child: ButtonTheme(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(0),
+                        ),
+                        child: FlatButton(
+                          onPressed: () {
+                            switch(menu[index]) {
+                              case "일기 작성" :
+                                Navigator.push(context, MaterialPageRoute(builder: (context) => WriteDiary()));
+                                break;
+                              case "그림일기 작성" :
+                                Navigator.push(context, MaterialPageRoute(builder: (context) => WritePictureDiary()));
+                                break;
+                              case "일기 목록" :
+                                break;
+                              case "커뮤니티" :
+                                break;
+                              case "설정" :
+                                break;
+                            }
+                          },
+                          color: Colors.amber,
+                          child: Text(
+                            "${menu[index]}",
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 11,
+                            ),
+                            textAlign: TextAlign.center,
                           ),
-                          textAlign: TextAlign.center,
                         ),
                       ),
-                    ),
-                  );
-                },
+                    );
+                  },
 
-                separatorBuilder: (BuildContext context, int index) {
-                  return Container(
-                    margin: const EdgeInsets.fromLTRB(0, 8, 0, 8),
-
-                    child: VerticalDivider(
-                      width: 2,
-                      thickness: 1.5,
-                      color: Colors.black,
-                    ),
-                  );
-                },
+                  separatorBuilder: (BuildContext context, int index) {
+                    return Container(
+                      margin: const EdgeInsets.fromLTRB(0, 8, 0, 8),
+                      child: VerticalDivider(
+                        width: 2,
+                        thickness: 1.5,
+                        color: Colors.black,
+                      ),
+                    );
+                  },
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         )
     );
   }
--- lib/WriteDiary.dart
+++ lib/WriteDiary.dart
@@ -1,9 +1,63 @@
 import 'package:flutter/material.dart';
 
-class WriteDiary {
+/**
+ * 차후 프래그먼트 형식으로 바꿀 때 참고할 사이트
+ * https://stackoverflow.com/questions/50640182/replace-widgets-like-fragments-in-flutter
+ * https://theunclesoon.blogspot.com/2020/06/flutter-route.html
+ **/
 
+class WriteDiary extends StatefulWidget{
+  @override
+  _WriteDiaryState createState() => _WriteDiaryState();
 }
 
-class WritePictureDiary {
+class _WriteDiaryState extends State<WriteDiary> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("일기 작성"),
+      ),
+      body: Container(
+        child: Column(
+          children: [
+            Container (
+              margin: const EdgeInsets.fromLTRB(125, 25, 125, 25),
+              child: TextField(
+                textAlign: TextAlign.center,
+                decoration: InputDecoration(
+                    hintText: "제목",
+                    hintStyle: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    )
+                ),
+              ),
+            ),
 
+            Expanded(child: Container(
+              margin: const EdgeInsets.only(left: 30, right: 30),
+              child: TextField(
+                keyboardType: TextInputType.multiline,
+                maxLines: null,
+                decoration: InputDecoration(),
+              ),
+            ))
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class WritePictureDiary extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Column(
+
+        ),
+      ),
+    );
+  }
 }
