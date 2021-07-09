a9af0031eacead11144e7ddc40d5fd09b039b3bf
DaeSeok_Song <zxdsaqwe1231@naver.com>
2021-07-09 19:58:27 +0900

    Write(Picture)Diary, Early Version complete

--- lib/WriteDiary.dart
+++ lib/WriteDiary.dart
@@ -11,6 +11,11 @@ class WriteDiary extends StatefulWidget{
   _WriteDiaryState createState() => _WriteDiaryState();
 }
 
+class WritePictureDiary extends StatefulWidget{
+  @override
+  _WritePictureDiaryState createState() => _WritePictureDiaryState();
+}
+
 class _WriteDiaryState extends State<WriteDiary> {
   @override
   Widget build(BuildContext context) {
@@ -39,9 +44,46 @@ class _WriteDiaryState extends State<WriteDiary> {
               child: TextField(
                 keyboardType: TextInputType.multiline,
                 maxLines: null,
+
                 decoration: InputDecoration(),
               ),
-            ))
+            )),
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+
+              children: [
+                Container(
+                  margin: const EdgeInsets.all(8),
+                  
+                  child: FloatingActionButton(
+                    child: Text(
+                      "돌아가기",
+                      style: TextStyle(
+                        fontSize: 13,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                  ),
+                ),
+
+                Container(
+                  margin: const EdgeInsets.all(8),
+
+                  child: FloatingActionButton(
+                    child: Icon(Icons.plus_one),
+
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                  ),
+                ),
+              ],
+            ),
           ],
         ),
       ),
@@ -49,13 +91,95 @@ class _WriteDiaryState extends State<WriteDiary> {
   }
 }
 
-class WritePictureDiary extends StatelessWidget{
+class _WritePictureDiaryState extends State<WritePictureDiary> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text("일기 작성"),
+      ),
       body: Container(
         child: Column(
+          children: [
+            Container(
+              margin: const EdgeInsets.fromLTRB(50, 25, 50, 10),
 
+              child: Center(
+                child: SizedBox(
+                  width: MediaQuery.of(context).size.width - 100,
+                  height: 200,
+                ),
+              ),
+
+              decoration: BoxDecoration(
+                border: Border.all(
+                  width: 1,
+                ),
+                borderRadius: BorderRadius.all(
+                    Radius.circular(5.0),
+                )
+              ),
+            ),
+
+            Container (
+              margin: const EdgeInsets.fromLTRB(125, 25, 125, 25),
+              child: TextField(
+                textAlign: TextAlign.center,
+                decoration: InputDecoration(
+                    hintText: "제목",
+                    hintStyle: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    )
+                ),
+              ),
+            ),
+
+            Expanded(child: Container(
+              margin: const EdgeInsets.only(left: 30, right: 30),
+              child: TextField(
+                keyboardType: TextInputType.multiline,
+                maxLines: null,
+
+                decoration: InputDecoration(),
+              ),
+            )),
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+
+              children: [
+                Container(
+                  margin: const EdgeInsets.all(8),
+
+                  child: FloatingActionButton(
+                    child: Text(
+                      "돌아가기",
+                      style: TextStyle(
+                        fontSize: 13,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                  ),
+                ),
+
+                Container(
+                  margin: const EdgeInsets.all(8),
+
+                  child: FloatingActionButton(
+                    child: Icon(Icons.plus_one),
+
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                  ),
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );
